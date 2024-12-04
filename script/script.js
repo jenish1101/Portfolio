8f5cb91f68be9221f551feb3a5cdafc4e0837f95
Jenish Gondaliya <jgondaliya28@gmail.com>
2024-12-04 14:04:02 +0530

    first commit

---
+++ script/script.js
@@ -0,0 +1,16 @@
+// add class navbarDark on navbar scroll
+// const header = document.querySelector('.navbar');
+// const text = document.querySelector(".nav-link");
+
+// window.onscroll = function() {
+//     var top = window.scrollY;
+//     if(top >=100) {
+//         header.classList.add('navbarDark');
+//         text.classList.add("text-clr");
+//     }
+//     else {
+//         header.classList.remove('navbarDark');
+//         text.classList.remove("text-clr")
+//     }
+// }
+
